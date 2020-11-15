dfaafcb1ebcbfcde10bdec4df86eaa9a3ef01add
Abdulaziz-04 <suria.abdul04@gmail.com>
2020-11-15 23:10:26 +0530

    Chrome Extension

---
+++ loader.js
@@ -0,0 +1,6 @@
+var w = window.innerWidth
+var h = window.innerHeight
+    //Substitute URL https://source.unsplash.com/random?{keywords}
+document.body.style.backgroundImage = `url("https://source.unsplash.com/collection/82878986/${w}x${h}")`
+document.body.style.backgroundRepeat = "no-repeat"
+document.body.style.backgroundSize = "contain"
